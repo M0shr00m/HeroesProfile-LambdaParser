06fd66f20410759d299aecc9782588f868a271f6
Zemill <33404722+Zemill@users.noreply.github.com>
2020-01-19 10:33:06 -0500

    fix: rebase

---
+++ Heroes.ReplayParser/Heroes.ReplayParser/BitReader.cs
@@ -0,0 +1,204 @@
+﻿namespace Heroes.ReplayParser.Streams
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// A basic little-endian bitstream reader.
+    /// </summary>
+    public class BitReader
+    {
+        public Stream stream;
+
+        private int currentByte;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BitReader"/> class.
+        /// </summary>
+        /// <param name="stream"> The stream. </param>
+        public BitReader(Stream stream)
+        {
+            this.stream = stream;
+            Cursor = 0;
+        }
+
+        /// <summary>
+        /// Gets the current cursor position.
+        /// </summary>
+        public int Cursor { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the end of stream has been reached.
+        /// </summary>
+        public bool EndOfStream {
+            get {
+                return (Cursor >> 3) == stream.Length;
+            }
+        }
+
+        /// <summary>
+        /// Reads up to 32 bits from the stream, returning them as a uint.
+        /// </summary>
+        /// <param name="numBits">
+        /// The number of bits to read.
+        /// </param>
+        /// <returns>
+        /// The <see cref="uint"/>.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if numBits is greater than 32.
+        /// </exception>
+        public uint Read(int numBits)
+        {
+            if (numBits > 32)
+            {
+                throw new ArgumentOutOfRangeException("numBits", "Number of bits must be less than 32.");
+            }
+
+            uint value = 0;
+
+            while (numBits > 0)
+            {
+                var bytePos = Cursor & 7;
+                int bitsLeftInByte = 8 - bytePos;
+                if (bytePos == 0)
+                {
+                    currentByte = stream.ReadByte();
+                }
+
+                var bitsToRead = (bitsLeftInByte > numBits) ? numBits : bitsLeftInByte;
+
+                value = (value << bitsToRead) | ((uint)currentByte >> bytePos) & ((1u << bitsToRead) - 1u);
+                Cursor += bitsToRead;
+                numBits -= bitsToRead;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Skip specified number of bits in stream.
+        /// </summary>
+        /// <param name="numBits">The number of bits to skip.</param>
+        public void Skip(int numBits)
+        {
+            // todo: calculade number of bytes to skip and just increment this.stream position
+            while (numBits > 0)
+            {
+                var bytePos = Cursor & 7;
+                int bitsLeftInByte = 8 - bytePos;
+                if (bytePos == 0)
+                {
+                    currentByte = stream.ReadByte();
+                }
+
+                var bitsToRead = (bitsLeftInByte > numBits) ? numBits : bitsLeftInByte;
+
+                Cursor += bitsToRead;
+                numBits -= bitsToRead;
+            }
+        }
+
+        /// <summary>
+        /// If in the middle of a byte, moves to the start of the next byte.
+        /// </summary>
+        public void AlignToByte()
+        {
+            if ((Cursor & 7) > 0)
+            {
+                Cursor = (Cursor & 0x7ffffff8) + 8;
+            }
+        }
+
+        /// <summary>
+        /// Reads up to 32 bits from the stream
+        /// </summary>
+        /// <param name="numBits">Number of bits to read, up to 32.</param>
+        /// <returns>Returns a uint containing the number of bits read.</returns>
+        public uint Read(uint numBits) => Read((int)numBits);
+
+        public bool[] ReadBitArray(uint numBits)
+        {
+            var bitArray = new bool[numBits];
+            for (var i = 0; i < bitArray.Length; i++)
+                bitArray[i] = ReadBoolean();
+
+            return bitArray;
+        }
+
+        /// <summary>
+        /// Reads 1 byte from the current stream position.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="byte"/>.
+        /// </returns>
+        public byte ReadByte() => (byte)Read(8);
+
+        /// <summary>
+        /// Reads a single bit from the stream as a boolean.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool ReadBoolean() => Read(1) == 1;
+
+        /// <summary>
+        /// Reads 2 bytes from the stream as a short.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="short"/>.
+        /// </returns>
+        public short ReadInt16() => (short)Read(16);
+
+        /// <summary>
+        /// Reads 4 bytes from the stream as an int.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        public int ReadInt32() => (int)Read(32);
+
+        /// <summary>
+        /// Reads an array of bytes from the stream.
+        /// </summary>
+        /// <param name="bytes">
+        /// The number of bytes to read.
+        /// </param>
+        /// <returns>
+        /// The <see cref="byte"/> array.
+        /// </returns>
+        public byte[] ReadBytes(int bytes)
+        {
+            var buffer = new byte[bytes];
+            for (int i = 0; i < bytes; i++)
+            {
+                buffer[i] = ReadByte();
+            }
+
+            return buffer;
+        }
+
+        /// <summary>
+        /// Reads a given number of bytes, parsing them as a UTF8 string.
+        /// </summary>
+        /// <param name="length">
+        /// The string length.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string ReadString(int length)
+        {
+            var buffer = ReadBytes(length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        public byte[] ReadBlobPrecededWithLength(int numBitsForLength)
+        {
+            var stringLength = Read(numBitsForLength);
+            AlignToByte();
+            return ReadBytes((int)stringLength);
+        }
+    }
+}
---
+++ hots-lambda/Function.cs
@@ -0,0 +1,118 @@
+using Amazon.Lambda.Core;
+using AWSSignatureV4_S3_Sample.Signers;
+using Heroes.ReplayParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace hotslambda
+{
+    public class Function
+    {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public Object FunctionHandler(IDictionary<string, string> dict, ILambdaContext context)
+        {
+            return MainAsync(dict["input"], dict["access"], dict["secret"]).GetAwaiter().GetResult();
+        }
+
+        static readonly string Prefix = "http://hotsapi.s3.amazonaws.com/";
+
+        public static async System.Threading.Tasks.Task<Object> MainAsync(string uri, string AWSAccessKey, string AWSSecretKey)
+        {
+            var sp = uri.Split('/');
+            uri = Prefix + sp.Last();
+
+            // for a simple GET, we have no body so supply the precomputed 'empty' hash
+            var headers = new Dictionary<string, string>
+            {
+                {AWS4SignerBase.X_Amz_Content_SHA256, AWS4SignerBase.EMPTY_BODY_SHA256},
+                {"content-type", "text/plain"},
+                {"x-amz-request-payer", "requester"}
+            };
+
+            var signer = new AWS4SignerForAuthorizationHeader
+            {
+                EndpointUri = new Uri(uri),
+                HttpMethod = "GET",
+                Service = "s3",
+                Region = "eu-west-1"
+            };
+
+            var authorization = signer.ComputeSignature(headers,
+                "",   // no query parameters
+                AWS4SignerBase.EMPTY_BODY_SHA256,
+                AWSAccessKey,
+                AWSSecretKey);
+
+            // place the computed signature into a formatted 'Authorization' header 
+            // and call S3
+            headers.Add("Authorization", authorization);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+            foreach (var header in headers.Keys)
+            {
+                request.Headers[header] = headers[header];
+            }
+            
+            byte[] bytes;
+            using (var response = await request.GetResponseAsync())
+            using (var stream = response.GetResponseStream())
+            using (var dst = new MemoryStream())
+            {
+                stream.CopyTo(dst);
+                bytes = dst.ToArray();
+            }
+            
+            var result = DataParser.ParseReplay(bytes, ParseOptions.MediumParsing);
+            if (result.Item1 != DataParser.ReplayParseResult.Success || result.Item2 == null)
+            {
+                return $"Error parsing replay: {result.Item1}";
+            }
+            return ToJson(result.Item2);
+        }
+
+        public static Object ToJson(Replay replay)
+        {
+            var obj = new
+            {
+                mode = replay.GameMode.ToString(),
+                region = replay.Players[0].BattleNetRegionId,
+                date = replay.Timestamp,
+                length = replay.ReplayLength,
+                map = replay.Map,
+                map_short = replay.MapAlternativeName,
+                version = replay.ReplayVersion,
+                version_major = replay.ReplayVersionMajor,
+                version_build = replay.ReplayBuild,
+                bans = replay.TeamHeroBans,
+                draft_order = replay.DraftOrder,
+                team_experience = replay.TeamPeriodicXPBreakdown,
+                players = from p in replay.Players
+                          select new
+                          {
+                              battletag_name = p.Name,
+                              battletag_id = p.BattleTag,
+                              blizz_id = p.BattleNetId,
+                              account_level = p.AccountLevel,
+                              hero = p.Character,
+                              hero_level = p.CharacterLevel,
+                              hero_level_taunt = p.HeroMasteryTiers,
+                              team = p.Team,
+                              winner = p.IsWinner,
+                              silenced = p.IsSilenced,
+                              party = p.PartyValue,
+                              talents = p.Talents.Select(t => t.TalentName),
+                              score = p.ScoreResult,
+                              staff = p.IsBlizzardStaff
+                          }
+            };
+            return obj;
+        }
+    }
+}
